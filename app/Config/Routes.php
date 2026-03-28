47ab56e665f4ce20e62dc93e83bc83a638ab22fc
bista11 <S.Bista11@wlv.ac.uk>
2026-03-28 19:56:03 +0545

    update: modify routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,20 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Trips::index');
+
+$routes->group('trips', static function ($routes) {
+    $routes->get('/', 'Trips::index');
+    $routes->get('create', 'Trips::create');
+    $routes->post('store', 'Trips::store');
+    $routes->get('edit/(:num)', 'Trips::edit/$1');
+    $routes->post('update/(:num)', 'Trips::update/$1');
+    $routes->get('delete/(:num)', 'Trips::delete/$1');
+});
+
+$routes->group('api', static function ($routes) {
+    $routes->get('destination-suggest', 'TripApi::destinationSuggest');
+    $routes->get('weather', 'TripApi::weather');
+    $routes->get('trips/search', 'TripApi::liveSearch');
+    $routes->get('nearby-trips', 'TripApi::nearbyTrips');
+});
